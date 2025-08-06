6c3ebb96c86bc131d792ce8c90eceb3249498287
Eric Chen <airkro@qq.com>
2025-08-06 22:14:26 +0800

    feat: core patch

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,14 @@ import base from '@nice-move/all-in-base/eslint';
 export default [
   ...base,
   {
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        getApp: 'readonly',
+      },
+    },
+  },
+  {
     ignores: ['src/**', 'fake/**', 'tests/fixtures/*'],
   },
 ];
